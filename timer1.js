a8a92fd78e8968c51658d4bc6d6bd7ab378238cf
Walid <walid_douri@hotmail.com>
2021-11-02 22:14:26 -0400

    Clean up

--- timer1.js
+++ timer1.js
@@ -19,33 +19,4 @@ const delayedAlerts = () => {
   }
 }
  
-
 delayedAlerts()
-
-// remove strings 
-//console.log(numbers)
-
-// let delay = 100;
-
-// spinyThing.forEach((spinyThing) => {
-//     setTimeout(() => {
-//         process.stdout.write(spinyThing);
-//     }, delay);
-//     delay += 200;
-// })
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
-
-// node timer1.js 10 3 5 15 9 
-
-/*
-For this app, we can think of at least these three:
-
-No numbers are provided: Easy. It should just not beep at all and end immediately since no beeps should get scheduled.
-  -> If input = null, then return null (null being no beeps)
-An input is a negative number: Ignore/skip any numbers that are negative. We can't schedule anything in the past.
-  -> if input =< 0, then return null(null being no beeps)
-An input is not a number: Ignore/skip these as well, instead of attempting to call setTimeout with a non-number.
-
-
-*/
